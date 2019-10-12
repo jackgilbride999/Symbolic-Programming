54de1e1d38a2e21c60dee41d402ce3212ce3cc84
jackgilbride999 <jackgilbride999@gmail.com>
2019-10-12 13:01:53 +0100

    add2 works successfully for given test cases

--- assignment1.pl
+++ assignment1.pl
@@ -7,9 +7,11 @@ add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
 
 % Helper predicate simp(X, Z)
-simp(X, 0) :- X = 0.                % 0 simplified is just 0
-simp(X+0, Z) :- simp(X, Z).         % X+0 simplified is the simplification of X
+simp(0, 0).                         % 0 simplified is just 0
+simp(0+X, Z) :- simp(X, Z).         % 0 added to X is just the simplification of X
+simp(X+0, Z) :- simp(X, Z).         % 0 added to X is just the simplification of X
 simp(X+s(Y), Z) :- simp(s(X)+Y, Z). % For X+Y, subtract one from Y, add it to X, and resimplify
-simp(X, X).                         % When X is not 0 or of the form X+Y, simp(X) is X
+simp(s(X), s(Z)) :- simp(X, Z).     % When X is not 0 or of the form X+Y, simp(X) is X
 
-% Define predicate add2(X, Y, Z)
+% Define predicate add2(X, Y, Z), where we simplify x, simplify y, and add
+add2(X, Y, Z) :- simp(X, SimpX), simp(Y, SimpY), add(SimpX, SimpY, Z).
