1ea7d4e8ecf0566fd263ecbb1d431b5ffc0439d9
Jack Gilbride <jackgilbride999@gmail.com>
2019-11-16 15:38:03 +0000

    Added problem statements for assignment 2

---
+++ assignment2.pl
@@ -0,0 +1,21 @@
+/*  Write a DCG that accepts strings of the form u2v where 
+    u and v are strings over the alphabet {0, 1} such that
+    the number of 0's in u is twice the number of 1's in v.
+*/
+% s1/2
+
+/*  Three neighbouring houses that all have a different 
+    colour, namely red, blue, and green. People of different 
+    nationalities live in the different houses and they all 
+    have a different pet. Write a DCG that outputs strings
+    [Col1,Nat1,Pet1, Col2,Nat2,Pet2, Col3,Nat3,Pet3] 
+    satisfying this condition where the nationalities are
+    english, spanish, japanese and the pets are jaguar, 
+    snail and zebra.
+*/
+% s2/2
+
+/*  Write a DCG that given a non-negative integer Sum, 
+    accepts a list of integers >= 1 that add up to Sum.
+*/
+% s3/3
