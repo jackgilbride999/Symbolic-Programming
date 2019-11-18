303c24ec020a9a645bac16679f495887b9b09cc4
Jack Gilbride <jackgilbride999@gmail.com>
2019-11-18 19:21:42 +0000

    Created helper predicate mkList/2

--- assignment2/assignment2c.pl
+++ assignment2/assignment2c.pl
@@ -1,3 +1,8 @@
 /*  Write a DCG that given a non-negative integer Sum, 
     accepts a list of integers >= 1 that add up to Sum.
 */
+
+
+% helper predicate mkList
+mkList(1, [1]).
+mkList(H, [H|T]) :- H \= 0, Hminus is H-1, mkList(Hminus, T).
