c0ab4a6a0d9751c15aa67524f09b6014edf8453f
Jack Gilbride <jackgilbride999@gmail.com>
2019-11-18 20:35:12 +0000

    Assignment 3 part 3 complete and passes unit tests

--- assignment2/assignment2c.pl
+++ assignment2/assignment2c.pl
@@ -1,8 +1,9 @@
 /*  Write a DCG that given a non-negative integer Sum, 
     accepts a list of integers >= 1 that add up to Sum.
 */
+s(0) --> [].
+s(Sum) --> [Digit], {mkList(Sum, L), member(Digit, L), Acc is Sum-Digit, Acc >= 0}, s(Acc).
 
-
-% helper predicate mkList
+% predicate mkList
 mkList(1, [1]).
 mkList(H, [H|T]) :- H \= 0, Hminus is H-1, mkList(Hminus, T).
--- assignment2/assignment2c.plt
+++ assignment2/assignment2c.plt
@@ -1,3 +1,8 @@
 :- begin_tests(assignment2c).
 
 :- include(assignment2c).
+
+test(mkList, all(L=[[3,2,1]])) :- mkList(3,L).
+test(s, all(L==[[3], [2,1], [1,2], [1,1,1]])) :- s(3,L,[]).
+
+:- end_tests(assignment2c).
