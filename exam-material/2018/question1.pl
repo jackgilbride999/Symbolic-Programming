a38e19d8695756201dcad5aabd4cf10805250719
Jack Gilbride <jackgilbride999@gmail.com>
2019-12-10 11:59:00 +0000

    Completed 2018 question 1

--- exam-material/2018/question1.pl
+++ exam-material/2018/question1.pl
@@ -14,7 +14,7 @@ iv)     ?- assert(love(john, mary))
         true
 
 v)      ?- .([], .(a,Y)) = [X, a]
-        X = [], Y = []  ? 
+        Y = [], X = [] 
 
 vi)     ?- setof(X,X=X,L)
         L = [_9930]
@@ -29,7 +29,7 @@ ix)     ?- X\=2, X=1
         false
 
 x)      ?- X < 2
-        error
+        instantiation error
 */
 
 /*
@@ -52,5 +52,55 @@ ii)     ?- q(X), q(Y).
         X = b; Y = b;
 
 iii)    ?- q(X), !, q(Y).
+        X = Y, Y = a;
+        X = a, Y = b.
         
+iv)     ?- q(c).
+        true.
 */
+
+/*
+c)  Given unary predicates bird, fly, penguin, write a Prolog program saying all birds fly except penguins (which do not).
+*/
+    fly(X) :- bird(X), \+ penguin(X).
+
+/*
+d)  Recall that positive integers can be encoded as successors of 0 (with, for example, succ(succ(0)) encoding 2),
+    and similarly, negative integers can be encoded as predecessors of 0 (with, for example, pred(pred(0)) encoding -2).
+*/
+    pos(succ(0)).
+    pos(succ(X)) :- pos(X).
+
+    neg(pred(0)).
+    neg(pred(X)) :- neg(X).
+
+/*
+    These definitions suggest two ways to represent all integers, given by the predicates pure and mixed below.
+*/
+    pure(0).
+    pure(X) :- pos(X); neg(X).
+
+    mixed(0).
+    mixed(succ(X)) :- mixed(X).
+    mixed(pred(X)) :- mixed(X).
+
+/*
+i)  Give a term t without variables such that Prolog answers yes/true to the following query
+        ?- mixed(t), \+ pure(t).
+        pred(succ(0)).
+*/
+
+/*
+ii) State Prolog's response to the query
+        ?- mixed(X), \+ pure(X).
+        program runs forever
+*/
+
+/*
+iii) Define a binary predicate convert(Mixed, Pure) that converts a mixed representation to its pure counterpart (representing the same integer)
+*/
+    convert(0, 0).
+    convert(s(p(X)), Y) :- convert(X, Y).
+    convert(p(s(X)), Y) :- convert(X,Y).
+    convert(s(X), s(Y)) :- convert(X, Y).
+    convert(p(X), p(Y)) :- convert(X, Y).
