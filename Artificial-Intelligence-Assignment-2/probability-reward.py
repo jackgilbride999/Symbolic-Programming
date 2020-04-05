b6c24b5a34bfbd69df9f6b8c70ba6424ccc9775f
Jack Gilbride <jackgilbride999@gmail.com>
2020-04-05 18:06:22 +0100

    Program working without accounting for dead state

--- Artificial-Intelligence-Assignment-2/probability-reward.py
+++ Artificial-Intelligence-Assignment-2/probability-reward.py
@@ -4,37 +4,16 @@
 # A = {exercise, relax}
 # on the basis of the following (probability, reward)-matrices
 # (p(s,a,s'), r(s,a,s')) for row s, column s' in table with corner a
-exercise = {
-    's_fit': {
-        'sdash_fit': [.99, 8],
-        'sdash_unfit': [.01, 8]
-    },
-    's_unfit': {
-        'sdash_fit': [.2, 0],
-        'sdash_unfit': [.8, 0]
-    }
-}
-
-relax = {
-    's_fit': {
-        'sdash_fit': [.7, 10],
-        'sdash_unfit': [.3, 10]
-    },
-    's_unfit': {
-        'sdash_fit': [0, 5],
-        'sdash_unfit': [1, 5]
-    }
-}
-# reworking this data structure?
 
 # for indexing into first level of list, i.e. parameter a
 EXERCISE = 0
 RELAX = 1
 
-# for indexing into second & third level of list, i.e 
+# for indexing into second & third level of list, i.e
 # parameters s & s'
 FIT = 0
 UNFIT = 1
+DEAD = 2
 
 plist = [
     [[.99, .01], [.2, .8]],
@@ -47,23 +26,47 @@ rlist = [
 ]
 
 
-# The γ-discounted value of (s, a) is
-# lim n->∞ qn(s,a)
-# where
-# q0(s,a) := p(s,a,fit)r(s,a,fit) + p(s,a,unfit)r(s,a,unfit)
 def p(s, a, sdash):
     return plist[a][s][sdash]
+
 
 def r(s, a, sdash):
     return rlist[a][s][sdash]
 
-def q0(s, a):
-    return p(s, a, FIT)*r(s, a, FIT) + p(s, a, UNFIT)*r(s,a,UNFIT)
+# The γ-discounted value of (s, a) is
+# lim n->∞ qn(s,a)
+# where
 
-def vn(s):
-    return max(qn((s, EXERCISE)), qn(s, RELAX))
 
-def qnplus(s,a):
-    return q0(s,a) + gamma(p(s,a,FIT)*vn(FIT) + p(s,a,UNFIT)*vn(UNFIT))
+def q(n, s, a):
+    if(n == 0):  # q0
+        return p(s, a, FIT)*r(s, a, FIT) + p(s, a, UNFIT)*r(s, a, UNFIT)
+    else:       # qn+1
+        return q(0, s, a) + gamma*(p(s, a, FIT)*v(n-1, FIT) + p(s, a, UNFIT)*v(n-1, UNFIT))
 
-print(p(FIT, RELAX, FIT))
+
+def v(n, s):  # vn
+    return max(q(n, s, EXERCISE), q(n, s, RELAX))
+
+
+while True:
+    try:
+        n = int(input("Please enter your positive integer n:"))
+        gamma = float(input("Please enter your γ-setting G (0<G<1):"))
+        if gamma <= 0 or gamma >= 1:
+            raise Exception
+        s = (input("Please enter your starting state s:"))
+        if s == "fit":
+            s = FIT
+        elif s == "unfit":
+            s = UNFIT
+        elif s == "dead":
+            s = DEAD
+        else:
+            raise Exception
+        exercise_output = q(n, s, EXERCISE)
+        relax_output = q(n, s, RELAX)
+        print("qn(s, exercise) = ", exercise_output)
+        print("qn(s, relax) = ", relax_output)
+    except:
+        print("Invalid inputs. Please try again.", gamma)
